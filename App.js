272b1dbc1297626f26afaec82d5b8b61f418d2b4
Helder Afonso <helderafonsoss@hotmail.com>
2019-08-27 20:54:55 -0300

    Gera a matriz responsavel pelo jogo e renderiza na ordem correta

--- App.js
+++ App.js
@@ -1,19 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+    const [game, setGame] = useState([]);
+
+    const startTable = () => {
+        let mat = new Array;
+        for (let i = 0; i < 3; i++) {
+            mat[i] = [i*3 + 1, i*3 + 2, i*3 + 3];
+        }
+        mat[2][2] = '';
+        setGame(mat);
+    }
+
+    useEffect(() => {
+        startTable();
+    }, [])
+
+    return (
+        <View style={styles.container}>
+            {
+                game.map((value, index) => {
+                    if(index <= 2){
+                        return(
+                            <View key={index} style={styles.line1}>
+                                <Text>{ value }</Text>
+                            </View>
+                        )
+                    }else if(index <= 5){
+                        return(
+                            <View key={index} style={styles.line2}>
+                                <Text>{ value }</Text>
+                            </View>
+                        )
+                    }else{
+                        return(
+                            <View key={index} style={styles.line3}>
+                                <Text>{ value }</Text>
+                            </View>
+                        )
+                    }
+                })
+            }
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
