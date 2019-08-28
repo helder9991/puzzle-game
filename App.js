3d6afc7a91fc555b8175b7b423190db07d309287
Helder Afonso <helderafonsoss@hotmail.com>
2019-08-27 23:34:23 -0300

    Logica do jogo completa

--- App.js
+++ App.js
@@ -4,13 +4,31 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 export default function App() {
 
     const [game, setGame] = useState([]);
+    const [size, setSize] = useState(3);
+
+    // Inicia matriz com numeros
+    const shuffle = () =>{
+        let aux = [...game];
+        console.log(game)
+        for (let i = 0; i < size*size; i++) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [aux[i], aux[j]] = [aux[j], aux[i]];
+        }
+        setGame(aux);
+    }
 
     const startTable = () => {
         let vet = new Array;
-        for (let i = 0; i < 9; i++) {
+        for (let i = 0; i < size*size; i++) {
             vet[i] = i+1;
         }
         vet[8] = '';
+
+        for (let i = 0; i < size*size; i++) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [vet[i], vet[j]] = [vet[j], vet[i]];
+        }
+
         setGame(vet);
     }
 
@@ -18,13 +36,44 @@ export default function App() {
         startTable();
     }, [])
 
+
+    const move = (position) => {
+        // Verifica se é possivel realizar o movimento
+        // Método escalonavel
+        let vet = [...game];
+
+        if(vet[position-1] === '' ){
+            aux = vet[position];
+            vet[position] = vet[position-1];
+            vet[position-1] = aux;
+        }
+        else if(vet[position+1] === ''){
+            aux = vet[position];
+            vet[position] = vet[position+1];
+            vet[position+1] = aux;
+        }
+        else if(vet[position-size] === ''){
+            aux = vet[position];
+            vet[position] = vet[position-size];
+            vet[position-size] = aux;
+        }
+        else if(vet[position+size] === ''){
+            aux = vet[position];
+            vet[position] = vet[position+size];
+            vet[position+size] = aux;
+        }
+
+        setGame(vet);
+        console.log(game);
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.gameContainer}>
             {
                 game.map((value, index) => (
                     <View key={index} style={styles.numberContainer}>
-                        <TouchableOpacity>
+                        <TouchableOpacity style={styles.numberButton} onPress={() => move(index)}>
                             <Text style={styles.number}>{value}</Text>
                         </TouchableOpacity>
                     </View>
@@ -50,6 +99,8 @@ const styles = StyleSheet.create({
         backgroundColor: '#fa8',
         flexDirection:'row',
         flexWrap: 'wrap', 
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     numberContainer:{
         width: '33%',
@@ -57,9 +108,12 @@ const styles = StyleSheet.create({
         backgroundColor: '#add',
         borderWidth: 1,
         borderColor: '#fff',
+    },
+    numberButton:{
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
-    },  
+    },
     number:{
         color: "#fff",
         fontSize: 36,
